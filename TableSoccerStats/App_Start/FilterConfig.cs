68979f02621188a632ad136b906577f28e0abd2e
Michal Guzowski <guzowski.michal@hotmail.com>
2016-12-18 02:43:26 +0100

    Add project files.

---
+++ TableSoccerStats/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TableSoccerStats
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
