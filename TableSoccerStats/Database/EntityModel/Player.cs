87d2e6d1ad4624810f2c258955d7d63ed422503a
Michal Guzowski <guzowski.michal@hotmail.com>
2016-12-18 04:29:54 +0100

    Add db connection

---
+++ TableSoccerStats/Database/EntityModel/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TableSoccerStats.Database.EntityModel
+{
+    public class Player
+    {
+    }
+}
---
+++ TableSoccerStats/Database/EntityModel/TssDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TableSoccerStats.Database.EntityModel
+{
+    public class TssDbContext : DbContext
+    {
+    }
+}
